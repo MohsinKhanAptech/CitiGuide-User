8524bd8d67b979f450f029b9899361600736d221
donatedsalt <mmohsinkhan3685@gmail.com>
2025-01-10 20:50:18 +0500

    completed help page

--- lib/pages/options_view_pages/help_page.dart
+++ lib/pages/options_view_pages/help_page.dart
@@ -5,6 +5,40 @@ class HelpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'For help please contact us through email or cell.',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.email_outlined),
+              SizedBox(width: 12),
+              Text(
+                'support@citiguide.com',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+          SizedBox(height: 12),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.phone_outlined),
+              SizedBox(width: 12),
+              Text(
+                '+92-300-1234567',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
