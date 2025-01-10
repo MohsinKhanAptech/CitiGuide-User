6a1c93e53a0b33d340b2db107f5a691e41c38d02
donatedsalt <mmohsinkhan3685@gmail.com>
2025-01-10 20:46:24 +0500

    completed about page

--- lib/pages/options_view_pages/about_page.dart
+++ lib/pages/options_view_pages/about_page.dart
@@ -5,6 +5,28 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 800),
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'CitiGuide',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'The City Guide mobile app is a digital platform that provides residents and tourists with information and recommendations for exploring a city, including attractions, restaurants, events, and accommodations. It\'s designed to be a user-friendly, all-in-one solution that enhances city exploration through mobile technology.',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
